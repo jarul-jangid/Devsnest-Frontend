fce1e6d29fa8faeab877cd24b172147992c1f0a3
jarul-jangid <jarul.ec19@nsut.ac.in>
2021-06-17 17:36:21 +0530

    Frontend Assignment

---
+++ THA 6/Assignment.js
@@ -0,0 +1,95 @@
+// 1. Write a JavaScript function to check whether an `input` is an array or not
+// Test Data :
+// console.log(is_array('w3resource'));
+// console.log(is_array([1, 2, 4, 0]));
+// false
+// True
+
+is_array = function(input) {
+    if (toString.call(input) === "[object Array]")
+      return true;
+    return false;   
+      };
+  console.log(is_array('w3resource'));
+  console.log(is_array([1, 2, 4, 0]));
+
+
+// 2. Write a JavaScript function to clone an array
+// Test Data :
+// console.log(array_Clone([1, 2, 4, 0]));
+// console.log(array_Clone([1, 2, [4, 0]]));
+// [1, 2, 4, 0]
+// [1, 2, [4, 0]]
+
+var array_Clone = (arr) => {
+    return arr.slice(0);
+};
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
+
+// 3. Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 
+//'n' elements of the array.
+// Test Data :
+// console.log(first([7, 9, 0, -2]));
+// console.log(first([],3));
+// console.log(first([7, 9, 0, -2],3));
+// console.log(first([7, 9, 0, -2],6));
+// console.log(first([7, 9, 0, -2],-3));
+// Expected Output :
+// 7
+// []
+// [7, 9, 0]
+// [7, 9, 0, -2]
+// []
+
+var first =  (array, n) => {
+    if (array == null) 
+    return void 0;
+  if (n == null) 
+    return array[0];
+  if (n < 0)
+    return [];
+  return array.slice(0, n);
+};
+
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+
+// 4. Write a simple JavaScript program to join all elements of the following array into a string.
+// Sample array : myColor = ["Red", "Green", "White", "Black"];
+// Expected Output :
+// "Red,Green,White,Black"
+// "Red,Green,White,Black"
+// "Red+Green+White+Black"
+
+var myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join("+"));
+
+// 5.  Write a JavaScript program to find the most frequent item of an array
+// Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+// Sample Output : a ( 5 times )
+
+const arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+let freq = 1;
+let m = 0;
+let item;
+for (let i=0; i<arr1.length; i++)
+{
+        for (let j=i; j<arr1.length; j++)
+        {
+                if (arr1[i] == arr1[j])
+                 m++;
+                if (freq<m)
+                {
+                  freq=m; 
+                  item = arr1[i];
+                }
+        }
+        m=0;
+}
+console.log(`${item} ( ${freq} times ) `) ;
